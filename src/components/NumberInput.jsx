7b8ecbb1c9465a56e1239e247cec0d5063f35c96
Farwavee <whatisafarwave@gmail.com>
2026-04-20 20:27:22 +0200

    Added Hooks for Input handel, error - false guess

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,5 +1,10 @@
-function NumberInput() {
-    return <p>Zahleneingabe</p>
+function NumberInput({ number, setNumber}) {
+    
+    const handleChange = (e) => setNumber(e.target.valueAsNumber);
+    return (
+        <input name="number" number={number} onChange={handleChange} />
+    )
+
 }
 
 export default NumberInput;
